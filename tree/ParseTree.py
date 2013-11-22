651ee226766150d08433899bd2ac6b112f8f5942
Chandan Kumar <chandan.gupta@gmail.com>
2013-11-22 16:53:27 +0530

    Initial import

---
+++ tree/ParseTree.py
@@ -0,0 +1,8 @@
+
+class ParseTreeDescription:
+    def __init__(self):
+        pass
+
+    def Parse(self, filename):
+        """ Returns a Tree object after parsing the file """
+        pass
---
+++ tree/Tree.py
@@ -0,0 +1,42 @@
+
+class Tree:
+    """ """
+    def __init__(self):
+        pass
+
+    def delete_node(self, value):
+        """ deletes entire sub-tree """
+        pass
+
+    def add_node(self, value, parent_node_ID):
+        pass
+
+    def find_nodes(self, value):  
+        """ Return an iterable list of Node objects """
+        pass
+
+    def replace(self, old_value, new_value):
+        pass
+
+    def replace_all(self, old_value, new_value):
+        pass
+
+    def child_nodes(self, value):
+        """ Return an iterable list of Node objects """
+        pass
+
+    def print_tree(self):
+        """ Write the tree structure in the format same as the defined input file """ 
+        pass
+
+    def print_visual_tree(self):
+        """ Print the tree in Linux "tree" format (run tree command to get an idea) """
+        pass
+
+    def depth(self):
+        """ Return the depth or height of the tree """
+        pass
+
+Node = Tree
+
+# iterable sub-tree
